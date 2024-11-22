7c9dc724a5fbe934c379c24353faa1b713dfbf70
Batman5054 <00001242021292sp@al.educacao.sp.gov.br>
2024-11-22 10:56:56 -0300

    MUDANÇAS

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1 mais 1?',
+    '2'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gato em Inglês?',
+    'Oi em ingles é CAT '
+)
